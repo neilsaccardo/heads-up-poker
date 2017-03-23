3cdbaaee1bcdecf7b051d1f7324d7442d4df88fa
Neil <neil.saccardo2@mail.dcu.ie>
2017-03-23 19:18:11 +0000

    modal stuff

--- src/ui/server/src/js/login.js
+++ src/ui/server/src/js/login.js
@@ -1,16 +1,40 @@
-function LoginController($scope, socket) {
+function LoginController($scope, socket, $uibModal) {
     var ctrl = this;
-
+    ctrl.showModal = false;
+    ctrl.username = ctrl.username || '';
     ctrl.submitUsername = function() {
         console.log('Log in requested...');
-        socket.emit('loginRequest', {id: ctrl.username});
+        if (badUsername()) {
+            console.log('Log in declined....');
+            $uibModal.open({templateUrl: 'js/modalContent.html'});
+        }
+        else {
+            socket.emit('loginRequest', {id: ctrl.username});
+            console.log('Log in accepted');
+        }
+
     }
+
+
+    function badUsername() {
+        console.log('ctrl.username ' + ctrl.username);
+        if (ctrl.username === '') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    ctrl.ok = function() {
+        console.log('Closed modal');
+    };
 };
 
-angular.module('login', ['socketService']).component('login', {
+angular.module('login', ['socketService', 'ui.bootstrap']).component('login', {
     templateUrl: 'js/login.html',
     controller: LoginController,
     bindings: {
         username: '<'
     }
 });
+
