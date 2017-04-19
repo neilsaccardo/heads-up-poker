3ff1f504bcc679ae7eaa8edafb37906ea40ddbcb
Neil <neil.saccardo2@mail.dcu.ie>
2017-04-19 17:32:48 +0100

    Redo preflop action determiner and add some unit tests

--- src/AI_Agent/src/main/java/com/saccarn/poker/ai/preflop/PreFlopDeterminer.java
+++ src/AI_Agent/src/main/java/com/saccarn/poker/ai/preflop/PreFlopDeterminer.java
@@ -11,45 +11,40 @@ import java.util.Random;
 public class PreFlopDeterminer {
 
     private Random randomGenerator = new Random();
-    private final int randomTopLimit = 100;
-    private final int randomisationThreshold = 95;
+    private final int RANDOM_TOP_LIMIT = 100;
+    private final int FOLD_THRESHOLD = 90;
+    private final int RANK_THRESHOLD = 40;
+    private int minBet;
+    private int stackSize;
+    private int position;
 
-
-    public String preFlopAction(String holeCard1, String holeCard2, int stackSize, int potSize, int position, int amountBet , Map<String, Double> playerCluster,
-                                 int minBet, int opponentStackSize) {
+    public String preFlopAction(String holeCard1, String holeCard2, int stackSize, int potSize, int position, int amountBet, Map<String, Double> playerCluster,
+                                int minBet, int opponentStackSize) {
+        this.minBet = minBet;
+        this.stackSize = stackSize;
+        this.position = position;
         String cards = HandRankings.transformCardsForHandRanking(holeCard1, holeCard2);
         HandRankings hrs = new HandRankings(); //(100-playerCluster.get(DataLoaderStrings.FOLDED_AT_PRE_FLOP))
-        if (hrs.getEVRankOfCardPair(cards) < 70) {
-            if (position == 0) { //first - bet check etc.
-                if (randomGenerator.nextInt(randomTopLimit) < randomisationThreshold) {
-                    int randomNum = randomGenerator.nextInt(randomTopLimit);
-                    if (randomNum < randomTopLimit / 3 && stackSize > (2*minBet)) {
-                        return ActionStrings.ACTION_BET2;
-                    } else if (randomNum < ((randomTopLimit / 3) * 2) && stackSize > (minBet)) {
-                        return ActionStrings.ACTION_BET1;
-                    }
-                    else {
-                        return ActionStrings.ACTION_PASS;
-                    }
-                } else {
-                    return ActionStrings.ACTION_FOLD;
-                }
-            } else {
-                if (randomGenerator.nextInt(randomTopLimit) < randomisationThreshold) {
-                    int randomNum = randomGenerator.nextInt(randomTopLimit);
-                    if (randomNum < randomTopLimit / 3 && stackSize > (2*minBet)) {
-                        return ActionStrings.ACTION_RAISE2;
-                    } else if (randomNum < ((randomTopLimit / 3) * 2) && stackSize > (minBet)) {
-                        return ActionStrings.ACTION_RAISE1;
-                    }
-                    else {
-                        return ActionStrings.ACTION_PASS;
-                    }
-                } else {
-                    return ActionStrings.ACTION_FOLD;
-                }
-            }
+        double rank = hrs.getEVRankOfCardPair(cards);
+        int randomNum = randomGenerator.nextInt(RANDOM_TOP_LIMIT);
+        return action(rank, randomNum);
+    }
+
+    public String action(double rank, int randomNumFoldOrContinue) {
+        // fold x% of the time when rank is above certain value.
+        if (rank > RANK_THRESHOLD && randomNumFoldOrContinue > FOLD_THRESHOLD ) {
+            return ActionStrings.ACTION_FOLD;
         }
-        return ActionStrings.ACTION_PASS;
+
+        int randomNum = randomGenerator.nextInt(RANDOM_TOP_LIMIT);
+        if (randomNum < ((RANDOM_TOP_LIMIT / 3) * 2)) {
+            return ActionStrings.ACTION_PASS;
+        }
+        else if (randomNum < ((RANDOM_TOP_LIMIT / 4) * 3)) {
+            return ActionStrings.ACTION_BET2;
+        }
+        else {
+            return ActionStrings.ACTION_BET1;
+        }
     }
 }
